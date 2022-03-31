911720e7b03f58f7aded45b33af301962bab0152
vcku <93396438+vcku@users.noreply.github.com>
2022-03-31 12:51:50 +0000

    cifs: fix potential buffer overrun when composing a new options string

--- plugin/highlight/highlight.js
+++ plugin/highlight/highlight.js
@@ -1,3 +1,9 @@
 // START CUSTOM REVEAL.JS INTEGRATION
 (function() {
-	if( typeof window.addEventListener ===
+	if( typeof window.addEventListener === 'function' ) {
+		var hljs_nodes = document.querySelectorAll( 'pre code' );
+
+		for( var i = 0, len = hljs_nodes.length; i < len; i++ ) {
+			var element = hljs_nodes[i];
+
+			// trim whitespace if data-trim attr
