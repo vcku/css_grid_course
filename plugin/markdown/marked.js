d29217db3fbc6f41d198be599e28ab0feb587541
vcku <93396438+vcku@users.noreply.github.com>
2022-10-20 19:57:13 +0000

    Remove debug code.

--- plugin/markdown/marked.js
+++ plugin/markdown/marked.js
@@ -1,2 +1,7 @@
 /**
- * marked - a ma
+ * marked - a markdown parser
+ * Copyright (c) 2011-2013, Christopher Jeffrey. (MIT Licensed)
+ * https://github.com/chjj/marked
+ */
+
+(function(){var block={newline:/^\n+/,code:/^( {4}[^\n]+\n*)+/,fences:noop,hr:/^( *[-*_]){3,} *(?:\n+|$)/,heading:/
