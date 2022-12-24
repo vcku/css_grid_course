a31c94f3d2f2c0b1f91f01e9ffed48fbc9409d39
vcku <93396438+vcku@users.noreply.github.com>
2022-12-24 20:16:15 +0000

    Sample/Editor.bjs

--- plugin/print-pdf/print-pdf.js
+++ plugin/print-pdf/print-pdf.js
@@ -2,4 +2,18 @@
  * phantomjs script for printing presentations to PDF.
  *
  * Example:
- * phantomjs print-pdf.js "http://lab.hakim.se/reveal-js?print
+ * phantomjs print-pdf.js "http://lab.hakim.se/reveal-js?print-pdf" reveal-demo.pdf
+ *
+ * By Manuel Bieh (https://github.com/manuelbieh)
+ */
+
+// html2pdf.js
+var page = new WebPage();
+var system = require( 'system' );
+
+page.viewportSize  = {
+	width: 1024,
+	height: 768
+};
+
+page.pap
