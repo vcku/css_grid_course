062167b869b77abdf599ce9d7a6041694599c8de
vcku <93396438+vcku@users.noreply.github.com>
2022-12-27 18:27:54 +0000

    nojekyll

--- plugin/zoom-js/zoom.js
+++ plugin/zoom-js/zoom.js
@@ -3,4 +3,11 @@
 	var isEnabled = true;
 
 	document.querySelector( '.reveal' ).addEventListener( 'mousedown', function( event ) {
-		if( event.altKey && isEnabled ) 
+		if( event.altKey && isEnabled ) {
+			event.preventDefault();
+			zoom.to({ element: event.target, pan: false });
+		}
+	} );
+
+	Reveal.addEventListener( 'overviewshown', function() { isEnabled = false; } );
+	Reveal.addEven
